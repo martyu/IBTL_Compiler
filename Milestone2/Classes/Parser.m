5992d2798e649dc5f397719b4196238a7bf268ee
David Merrick <david@david-merrick.com>
2014-02-16 21:11:25 -0800

    Made some updates to parser

--- Milestone2/Classes/Parser.m
+++ Milestone2/Classes/Parser.m
@@ -54,7 +54,6 @@
 
 #pragma mark - Productions
 
-
 - (void) start
 {
 	
@@ -137,7 +136,6 @@
     [self printsmts];
 }
 
-
 /** ifstmts -> [if expr expr expr] | [if expr expr] */
 -(void) ifstmts
 {
@@ -159,19 +157,46 @@
 /** varlist -> [name type] | [name type] varlist */
 -(void) varlist
 {
-    
+    if (self.lookAhead.tag == '[')
+	{
+		[self match:'['];
+        [self name];
+        if(self.lookAhead.tokType == TokenTypeType){
+            [self match:']'];
+        } else {
+            //@todo: error? Another varlist?
+        }
+
+    }
 }
 
 /** printstmts -> [stdout oper] */
 -(void) printsmts
 {
-    
+    if (self.lookAhead.tag == '[')
+	{
+		[self match:'['];
+        [self match:STDOUT];
+        [self oper];
+        [self match:']'];
+    } else {
+        //@todo: Error?
+    }
 }
 
-/** expr | expr exprlist */
+/** exprlist -> expr | expr exprlist */
 -(void) exprlist
 {
-    
+    //How to do this one?!
+    /*
+    if([self expr]){
+        return TRUE;
+    } else if([self expr] || [self exprlist]){
+        return true;
+     } else {
+        return false;
+     }
+     */
 }
 
 //- (void)program
