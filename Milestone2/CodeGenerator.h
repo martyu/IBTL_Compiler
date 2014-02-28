0945d2e40fdf071992f9e1a7816d6234b5f30bed
Marty Ulrich <marulrich@ebay.com>
2014-02-27 16:43:27 -0800

    added CodeGenerator class.

---
+++ Milestone2/CodeGenerator.h
@@ -0,0 +1,13 @@
+//
+//  CodeGenerator.h
+//  Milestone2
+//
+//  Created by Marty Ulrich on 2/27/14.
+//  Copyright (c) 2014 Marty Ulrich/David Merrick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CodeGenerator : NSObject
+
+@end
---
+++ Milestone2/CodeGenerator.m
@@ -0,0 +1,13 @@
+//
+//  CodeGenerator.m
+//  Milestone2
+//
+//  Created by Marty Ulrich on 2/27/14.
+//  Copyright (c) 2014 Marty Ulrich/David Merrick. All rights reserved.
+//
+
+#import "CodeGenerator.h"
+
+@implementation CodeGenerator
+
+@end
